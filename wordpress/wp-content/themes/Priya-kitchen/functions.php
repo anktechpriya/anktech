54ff0a075db2f739f31d6be433a03164103f3215
Priya <priya.kumari@anktech.co.in>
2022-03-31 20:29:54 +0530

    commit my custom theme

---
+++ wordpress/wp-content/themes/Priya-kitchen/functions.php
@@ -0,0 +1,16 @@
+<?php
+
+function custom_theme_assets() {
+	wp_enqueue_style( 'style', get_stylesheet_uri() );
+}
+
+add_action( 'wp_enqueue_scripts', 'custom_theme_assets' );
+
+register_nav_menus( [ 'primary' => __( 'Primary Menu' ) ] );
+
+function add_featured_image_support_to_your_wordpress_theme() {
+	add_theme_support( 'post-thumbnails' );
+    set_post_thumbnail_size( 400,400 );
+}
+
+add_action( 'after_setup_theme', 'add_featured_image_support_to_your_wordpress_theme' );
---
+++ wordpress/wp-content/themes/Priya-kitchen/home.php
@@ -0,0 +1,120 @@
+<?php
+/* Template Name: Home
+*/
+get_header();
+?>
+
+<div class = "hero">
+    <img src = "http://localhost/anktech/wordpress/wp-content/uploads/2022/03/food.jpg" style="width:100%;">
+    <div class="text-block">
+    <h4>Chennai kitchen</h4>
+    <p>I’ve grown up in the kitchen along side my<br> mum and grandmothers and conversations <br>in my family are always about the next meal.<br> I’ve picked up their love for food along the way, <br>and with this blog, I share my food story with you.</p>
+  </div>
+</div>
+
+<div>
+    <h3 class = "category"><span>Browse by category</span></h3>
+</div>
+
+<div class="container">
+  <div class="row">
+  <?php
+$args = array(
+    'post_type' => 'post',
+    'category_name' => 'chinese',
+    
+
+);
+
+$posts = new WP_Query($args);
+if($posts -> have_posts()) : ?>
+<?php while ($posts -> have_posts()) :
+$posts -> the_post(); ?>
+
+    <div class="col-md-3">
+      <div class="thumbnail">
+         <a href = "<?php echo get_the_permalink(get_the_ID()); ?>" > <?php the_post_thumbnail(150, 150); ?></a>
+      </div>
+          <div class="caption">
+          <h3> <a href = "<?php echo get_the_permalink(get_the_ID()); ?>" > <?php the_title(); ?></a></h3>
+          </div>
+        </a>
+      </div>
+    
+ 
+<?php endwhile;
+endif;
+?>
+</div>
+</div>
+<div>
+    <h3 class = "category"><span>Latest Recipes</span></h3>
+</div>
+<div>
+    <?php
+$args = array(
+    'post_type' => 'post',
+    'category_name' => 'italian',
+    
+
+);
+
+$posts = new WP_Query($args);
+if($posts -> have_posts()) : ?>
+<?php while ($posts -> have_posts()) :
+$posts -> the_post(); ?>
+
+    <div class="col-md-4">
+      <div class="thumbnail">
+         <a href = "<?php echo get_the_permalink(get_the_ID()); ?>" > <?php the_post_thumbnail(150, 150); ?></a>
+      </div>
+          <div class="caption">
+          <h3> <a href = "<?php echo get_the_permalink(get_the_ID()); ?>" > <?php the_title(); ?></a></h3>
+          </div>
+        </a>
+      </div>
+    
+ 
+<?php endwhile;
+endif;
+?>
+</div>
+
+<div>
+    <h3 class = "category"><span>Main Course</span></h3>
+</div>
+
+<div class="container">
+  <div class="row">
+  <?php
+$args = array(
+    'post_type' => 'post',
+    'category_name' => 'punjabi',
+    
+
+);
+
+$posts = new WP_Query($args);
+if($posts -> have_posts()) : ?>
+<?php while ($posts -> have_posts()) :
+$posts -> the_post(); ?>
+
+    <div class="col-md-3">
+      <div class="thumbnail">
+         <a href = "<?php echo get_the_permalink(get_the_ID()); ?>" > <?php the_post_thumbnail(150, 150); ?></a>
+      </div>
+          <div class="caption">
+          <h3> <a href = "<?php echo get_the_permalink(get_the_ID()); ?>" > <?php the_title(); ?></a></h3>
+          </div>
+        </a>
+      </div>
+    
+ 
+<?php endwhile;
+endif;
+?>
+</div>
+</div>
+
+<?php
+get_footer(); ?>
