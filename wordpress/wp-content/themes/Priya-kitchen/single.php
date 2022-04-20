a6310147407510d647b62b2c3824cc3010caa578
Priya Kumari <priya.kumari@anktech.co.in>
2022-04-20 18:19:50 +0530

    created widget

--- wordpress/wp-content/themes/Priya-kitchen/single.php
+++ wordpress/wp-content/themes/Priya-kitchen/single.php
@@ -1,41 +1,28 @@
 <?php
-
 get_header();
+?>
+<div class="breadcrumb"><?php get_breadcrumb(); ?></div>
+<h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+<?php
 
-if ( have_posts() ) :
-	while ( have_posts() ) : the_post(); ?>
+if ( have_posts()):
+    while ( have_posts()):
+         the_post(); ?>
+		<div class="row">
+    <div class="col-lg-4 pl-2 pr-2 latest-recipes_content">
+        <a href = "<?php echo get_the_permalink(get_the_ID()); ?>" > <?php the_post_thumbnail('full'); ?></a>
+    </div>
+    <div class="col-lg-4 pl-2 pr-2 latest-recipes_content"> 
+        <?php the_content(); ?>
+    </div>
+    </div>
+   <?php
+    endwhile;
+    
+else:
+    echo '<p>There are no posts!</p>';
 
-        <article class="post">
-			<?php if ( has_post_thumbnail() ) { ?>
-                <div class="single-post-image">
-					<?php the_post_thumbnail( 'single-post-image' ); ?>
-                </div>
-			<?php } ?>
-            <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
-            <p class="post-meta"><?php the_time( 'F jS, Y' ); ?> | <a
-                        href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' ) ); ?>"><?php the_author(); ?></a>
-                | <?php
-				$categories = get_the_category();
-				$comma      = ', ';
-				$output     = '';
-				
-				if ( $categories ) {
-					foreach ( $categories as $category ) {
-						$output .= '<a href="' . get_category_link( $category->term_id ) . '">' . $category->cat_name . '</a>' . $comma;
-					}
-					echo trim( $output, $comma );
-				} ?>
-            </p>
-			<?php the_content() ?>
-        </article>
-	
-	<?php endwhile;
+endif; ?>
+<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Custom Widgets') ) : ?> <?php endif; ?>
 
-else :
-	echo '<p>There are no posts!</p>';
-
-endif;
-
-get_footer();
-
-?> 
+<?php get_footer(); ?>
